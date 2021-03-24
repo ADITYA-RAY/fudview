546d06164c281e967ecdd5f245ccedff83357111
ADITYA-RAY <74045024+ADITYA-RAY@users.noreply.github.com>
2021-03-24 19:25:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -111,54 +111,90 @@ function openCart(dishname, price) {
         document.getElementById("table1").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
     }
     else if (tableno == 2) {
+        var total2 = document.getElementById("subtotal2").innerHTML;
+        var t2 = parseInt(total2);
+        t2 += parseInt(price);
+        document.getElementById("subtotal2").innerHTML = t2;
         document.getElementById("box2").style.backgroundColor = "red";
         document.getElementById("table2").style.display = "table";
         document.getElementById("table2").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
 
     }
     else if (tableno == 3) {
+        var total3 = document.getElementById("subtotal3").innerHTML;
+        var t3 = parseInt(total3);
+        t3 += parseInt(price);
+        document.getElementById("subtotal3").innerHTML = t3;
         document.getElementById("box3").style.backgroundColor = "red";
         document.getElementById("table3").style.display = "table";
         document.getElementById("table3").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
 
     }
     else if (tableno == 4) {
+        var total4 = document.getElementById("subtotal4").innerHTML;
+        var t4 = parseInt(total4);
+        t4 += parseInt(price);
+        document.getElementById("subtotal4").innerHTML = t4;
         document.getElementById("box4").style.backgroundColor = "red";
         document.getElementById("table4").style.display = "table";
         document.getElementById("table4").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
 
     }
     else if (tableno == 5) {
+        var total5 = document.getElementById("subtotal5").innerHTML;
+        var t5 = parseInt(total5);
+        t5 += parseInt(price);
+        document.getElementById("subtotal5").innerHTML = t5;
         document.getElementById("box5").style.backgroundColor = "red";
         document.getElementById("table5").style.display = "table";
         document.getElementById("table5").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
 
     }
     else if (tableno == 6) {
+        var total6 = document.getElementById("subtotal6").innerHTML;
+        var t6 = parseInt(total6);
+        t6 += parseInt(price);
+        document.getElementById("subtotal6").innerHTML = t6;
         document.getElementById("box6").style.backgroundColor = "red";
         document.getElementById("table6").style.display = "table";
         document.getElementById("table6").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
 
     }
     else if (tableno == 7) {
+        var total7 = document.getElementById("subtotal7").innerHTML;
+        var t7 = parseInt(total7);
+        t7 += parseInt(price);
+        document.getElementById("subtotal7").innerHTML = t7;
         document.getElementById("box7").style.backgroundColor = "red";
         document.getElementById("table7").style.display = "table";
         document.getElementById("table7").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
 
     }
     else if (tableno == 8) {
+        var total8 = document.getElementById("subtotal8").innerHTML;
+        var t8 = parseInt(total8);
+        t8 += parseInt(price);
+        document.getElementById("subtotal8").innerHTML = t8;
         document.getElementById("box8").style.backgroundColor = "red";
         document.getElementById("table8").style.display = "table";
         document.getElementById("table8").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
 
     }
     else if (tableno == 9) {
+        var total9 = document.getElementById("subtotal9").innerHTML;
+        var t9 = parseInt(total9);
+        t9 += parseInt(price);
+        document.getElementById("subtotal9").innerHTML = t9;
         document.getElementById("box9").style.backgroundColor = "red";
         document.getElementById("table9").style.display = "table";
         document.getElementById("table9").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
 
     }
     else if (tableno == 10) {
+        var total10 = document.getElementById("subtotal10").innerHTML;
+        var t10 = parseInt(total10);
+        t10 += parseInt(price);
+        document.getElementById("subtotal10").innerHTML = t10;
         document.getElementById("box10").style.backgroundColor = "red";
         document.getElementById("table10").style.display = "table";
         document.getElementById("table10").innerHTML += "<tr>" + "<td>" + dishname + "</td>" + "<td>" + "<button id='increase' onclick='increaseq(this,1)'>+</button><button id='decrease' onclick='decreaseq()'>-</button>" + "</td>" + "<td>" + "&#8377 " + price + "</td>" + "</tr>";
